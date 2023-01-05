cb9cd1a6b53585d732ca3ab93c1ea1e78ff562a3
vishw-jeet <122045875+vishw-jeet@users.noreply.github.com>
2023-01-05 23:53:42 +0530

    my 1st task

---
+++ assest/js/app.js
@@ -0,0 +1,48 @@
+cl= console.log;
+
+const showmodal= document.getElementById('showmodal');
+const backDrop = document.getElementById('backDrop');
+const mymodal = document.getElementById('mymodal');
+
+const addmovieBtn = document.getElementById('addmovieBtn');
+const titleControl = document.getElementById('title');
+const imgUrlControl = document.getElementById('imgurl');
+const ratingControl = document.getElementById('rating');
+
+
+
+const moviemodalClose = [ ...document.querySelectorAll ('.moviemodalClose')];
+
+const onMovieCardArray = [{
+    title:"",
+    imageUrl:"",
+    rating:"",
+}]
+
+const toggleMovieModal = () => {
+    backDrop.classList.toggle('show');
+    mymodal.classList.toggle('show');
+}
+
+moviemodalClose.forEach(closeEle => {
+    closeEle.addEventListener('click',toggleMovieModal);
+})
+
+const onMovieCardAdd =(ele) =>{
+    let movieObj = {
+        title: titleControl.value,
+        imageUrl: imgUrlControl.value,
+        Rating: ratingControl.value,
+    }
+    cl(movieObj)
+    onMovieArray.unshift(movieObj)
+    cl(onMovieArray)
+    titleControl.value="";
+    imgUrlControl.value="";
+    ratingControl.value="";
+    
+}
+
+showmodal.addEventListener('click',toggleMovieModal);
+
+addmovieBtn.addEventListener("click",onMovieCardAdd);
